919c0f8334ed1f7688a08cce57bb7b459839a6fd
farsider350 <31876349+farsider350@users.noreply.github.com>
2023-11-14 20:03:25 +1000

    Correct Serialization Methods - block.h

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -13,27 +13,15 @@
 #include <uint256.h>
 #include <util/time.h>
 
-/** Nodes collect new transactions into a block, hash them into a hash tree,
- * and scan through nonce values to make the block's hash satisfy proof-of-work
- * requirements.  When they solve the proof-of-work, they broadcast the block
- * to everyone and the block is added to the block chain.  The first transaction
- * in the block is a special one that creates a new coin owned by the creator
- * of the block.
- */
 class CBlockHeader : public CPureBlockHeader
 {
 public:
-
-    
-    // header
     int32_t nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
     uint32_t nTime;
     uint32_t nBits;
     uint32_t nNonce;
-
-    // auxpow (if this is a merge-minded block)
     std::shared_ptr<CAuxPow> auxpow;
 
     CBlockHeader()
@@ -41,13 +29,10 @@ public:
         SetNull();
     }
 
-    SERIALIZE_METHODS(CBlockHeader, obj) { READWRITE(obj.nVersion, obj.hashPrevBlock, obj.hashMerkleRoot, obj.nTime, obj.nBits, obj.nNonce); }
-
-    SERIALIZE_METHODS;
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action) {
+    SERIALIZE_METHODS(CBlockHeader, obj)
+    {
         READWRITE(*(CPureBlockHeader*)this);
+        READWRITE(obj.nVersion, obj.hashPrevBlock, obj.hashMerkleRoot, obj.nTime, obj.nBits, obj.nNonce);
 
         if (this->IsAuxpow())
         {
@@ -55,7 +40,8 @@ public:
                 auxpow = std::make_shared<CAuxPow>();
             assert(auxpow != nullptr);
             READWRITE(*auxpow);
-        } else if (ser_action.ForRead())
+        }
+        else if (ser_action.ForRead())
             auxpow.reset();
     }
 
@@ -70,11 +56,7 @@ public:
         nBits = 0;
     }
 
-    /**
-     * Set the block's auxpow (or unset it).  This takes care of updating
-     * the version accordingly.
-     */
-    void SetAuxpow (std::unique_ptr<CAuxPow> apow);
+    void SetAuxpow(std::unique_ptr<CAuxPow> apow);
 
     bool IsNull() const
     {
@@ -108,17 +90,11 @@ public:
     }
 };
 
-
 class CBlock : public CBlockHeader
 {
 public:
-    // network and disk
     std::vector<CTransactionRef> vtx;
-
-    // pos block signature
     std::vector<unsigned char> vchBlockSig;
-
-    // memory only
     mutable bool fChecked;
 
     CBlock()
@@ -146,7 +122,8 @@ public:
     {
         READWRITEAS(CBlockHeader, obj);
         READWRITE(obj.vtx);
-        if (obj.vtx.size() > 1 && obj.vtx[1]->IsCoinStake()) {
+        if (obj.vtx.size() > 1 && obj.vtx[1]->IsCoinStake())
+        {
             READWRITE(obj.vchBlockSig);
         }
     }
@@ -161,23 +138,19 @@ public:
     CBlockHeader GetBlockHeader() const
     {
         CBlockHeader block;
-        block.nVersion       = nVersion;
-        block.hashPrevBlock  = hashPrevBlock;
+        block.nVersion = nVersion;
+        block.hashPrevBlock = hashPrevBlock;
         block.hashMerkleRoot = hashMerkleRoot;
-        block.nTime          = nTime;
-        block.nBits          = nBits;
-        block.nNonce         = nNonce;
-        block.auxpow         = auxpow;
+        block.nTime = nTime;
+        block.nBits = nBits;
+        block.nNonce = nNonce;
+        block.auxpow = auxpow;
         return block;
     }
 
     std::string ToString() const;
 };
 
-/** Describes a place in the block chain to another node such that if the
- * other node doesn't have the same branch, it can find a recent common trunk.
- * The further back it is, the further before the fork it may be.
- */
 struct CBlockLocator
 {
     std::vector<uint256> vHave;
