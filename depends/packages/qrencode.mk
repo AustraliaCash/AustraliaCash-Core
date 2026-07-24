d9acb06ec46b9137f218eccf1c8e2031f2b05bc0
farsider350 <31876349+farsider350@users.noreply.github.com>
2026-07-25 08:05:11 +1000

    Update qrencode.mk

--- depends/packages/qrencode.mk
+++ depends/packages/qrencode.mk
@@ -8,16 +8,21 @@ $(package)_extract_cmds= \
   echo "$($(package)_sha256_hash)  $($(package)_source)" > $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   $(build_SHA256SUM) -c $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   tar --strip-components=1 -xf $($(package)_source) -C $($(package)_extract_dir)
+
 define $(package)_set_vars
-$(package)_config_opts=--disable-shared -without-tools --disable-sdltest
+$(package)_config_opts=--disable-shared --without-tools --disable-sdltest
 $(package)_config_opts_linux=--with-pic
+$(package)_preprocess_cmds=autoreconf -vfi
 endef
+
 define $(package)_config_cmds
   $($(package)_autoconf)
 endef
+
 define $(package)_build_cmds
   $(MAKE)
 endef
+
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
