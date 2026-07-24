389b3f9d91cdc3a1032348d288b85e8230af28e0
farsider350 <31876349+farsider350@users.noreply.github.com>
2026-07-24 20:58:41 +1000

    Update expat.mk

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,7 +1,7 @@
 package=expat
 $(package)_version=2.2.0
-$(package)_download_path=https://downloads.sourceforge.net/project/expat/expat/2.2.0/$($(package)_version)
-$(package)_file_name=$(package)-$($(package)_version).tar.bz2
+$(package)_download_path=https://toolchains.bootlin.com/downloads/releases/sources/expat-2.2.0
+$(package)_file_name=expat-2.2.0.tar.bz2
 $(package)_sha256_hash=d9e50ff2d19b3538bd2127902a89987474e1a4db8e43a66a4d1a712ab9a504ff
 
 define $(package)_set_vars
